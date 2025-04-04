dab74f346d3dee4e7a1a7b1c11c7d94a125bf724
smclacke <sarahmclacken@gmail.com>
2025-04-04 15:05:29 +0200

    now, prisma is the only thing creating the db, connecting the client and querying the db. we dont need all the sqlite and decorated fastify with db stuff. super simple and working nicely

--- src/database/dbConnector.ts
+++ src/database/dbConnector.ts
@@ -1,38 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-import Database from 'better-sqlite3';
 import fp from "fastify-plugin";
-import fs from 'fs';
-import path from 'node:path';
 import { FastifyInstance } from 'fastify';
 
-const databasePath = path.join(__dirname, '/database.db');
 const prisma = new PrismaClient();
 
-async function dbConnector(fastify: FastifyInstance): Promise<void> {
-	let db;
+async function dbConnector(fastify: FastifyInstance) {
 
-	if (fs.existsSync(databasePath)) {
-		console.log("Database already exists at: ", databasePath);
-		db = new Database(databasePath, { verbose: console.log });
-	}
-	else {
-		console.log("Creating new database at: ", databasePath);
-		db = new Database(databasePath, { verbose: console.log });
-
-		try {
-			await prisma.$queryRaw`SELECT 1`;
-			console.log("Created database successfully.");
-		} catch (error) {
-			console.error("Error initializing database:", error);
-		  }
-	}
-	fastify.decorate("db", db);
+	await prisma.$connect();
 	fastify.decorate("prisma", prisma);
-	prisma.$connect();
-	console.log("Fastify instance has 'db':", fastify.hasDecorator('db'));
-	console.log("Fastify instance has 'prisma':", fastify.hasDecorator('prisma'));
-	fastify.addHook("onClose", (fastify, done) => {
-		done();
+	fastify.addHook("onClose", async () => {
+		await prisma.$disconnect();
 	});
 };
 
--- src/types.d.ts
+++ src/types.d.ts
@@ -3,7 +3,6 @@ import Database from "better-sqlite3";
 
 declare module "fastify" {
 	interface FastifyInstance {
-		db: Database;
 		prisma: PrismaClient;
 	}
 	interface FastifyRequest {
