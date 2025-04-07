eeb44cb2de65b6d2e1a17d919eb3cefcb877f083
smclacke <sarahmclacken@gmail.com>
2025-04-07 19:59:41 +0200

    fixing login, cant see in which service the error is occuring

--- src/controllers/user/login.controller.ts
+++ src/controllers/user/login.controller.ts
@@ -11,24 +11,25 @@ export const login = async (request: FastifyRequest, reply: FastifyReply): Promi
 		const { username, password } = request.body as { username: string, password: string };
 		//if (!username || !password)
 				// how do we want to handle this? - frontend ?
-		console.log("herew in db login");
 		const user = await request.server.prisma.user.findUnique({
-			where: { username }
-		});
-		// if user not found, handle in frontend?
+			where: { username: username }});
+
+		if (user)
+			console.log("FOUND USER");
 		
-		const isPasswordValid = await request.server.prisma.user.compare(password, request.server.prisma.user.password);
-		if (!isPasswordValid)
-			reply.status(500).send({ error: 'wrong password' });
-		console.log("password check good");
-		await request.server.prisma.user.update({
-			where: {
-				username: user,
-			},
-			data: {
-				status: true
-			}
-			})
+		//console.log(`user pass = ${request.server.prisma.user.password}, pass = ${password}`);
+		//if (password !== request.server.prisma.user.password)
+		//	reply.status(500).send({ error: 'wrong password' });
+		//const isPasswordValid = await request.server.prisma.user.compare(password, request.server.prisma.user.password);
+		//if (!isPasswordValid)
+		//await request.server.prisma.user.update({
+		//	where: {
+		//		username: username
+		//	},
+		//	data: {
+		//		status: true
+		//	},
+		//});
 		
 		reply.code(201).send(user);
 	} catch (error) {
