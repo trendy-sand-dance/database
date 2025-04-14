37773f653eb358fdcd53e878fd240d5aa5cfc438
fvan-wij <vanwijkflip@gmail.com>
2025-04-14 18:25:32 +0200

    Can now get and update game player

--- src/controllers/user/register.controller.ts
+++ src/controllers/user/register.controller.ts
@@ -1,22 +1,26 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 
 export const register = async (request: FastifyRequest, reply: FastifyReply): Promise<any> => {
-	try {
-		const { username, password, email } = request.body as { username: string, password: string, email: string };
-		const { avatar, status } = { avatar: "img_avatar.png", status: false };
-	
-		await request.server.prisma.user.create({
-			data: {
-				username,
-				password,
-				email,
-				avatar,
-				status
-			},
-		});
-		return reply.code(201).send({ message: "Registration successful" });
-	} catch (error) {
-		console.error(error);
-		return reply.code(500).send({ error: "Failed to create new user" });
-	}
+  try {
+    const { username, password, email } = request.body as { username: string, password: string, email: string };
+    const { avatar, status } = { avatar: "img_avatar.png", status: false };
+
+    await request.server.prisma.user.create({
+      data: {
+        username,
+        password,
+        email,
+        avatar,
+        status,
+        player: {
+          create: {
+          }
+        }
+      },
+    });
+    return reply.code(201).send({ message: "Registration successful" });
+  } catch (error) {
+    console.error(error);
+    return reply.code(500).send({ error: "Failed to create new user" });
+  }
 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,51 +1,56 @@
 import { FastifyInstance } from 'fastify';
 
-	// dev
-	import {getHome} from "../controllers/dev/getHome.controller";
-	import {viewDB} from "../controllers/dev/userDev/viewDB.controller";
-	import {viewID} from "../controllers/dev/userDev/viewID.controller";
+// dev
+import { getHome } from "../controllers/dev/getHome.controller";
+import { viewDB } from "../controllers/dev/userDev/viewDB.controller";
+import { viewID } from "../controllers/dev/userDev/viewID.controller";
 
-	// web
-	import {dash} from "../controllers/web/dash.controller";
-	
-	// user endpoints
-	import {register} from "../controllers/user/register.controller";
-	import {login} from "../controllers/user/login.controller";
-	import {logout} from "../controllers/user/logout.controller";
-	import {editUsername} from "../controllers/user/edit.controller"
-	import {editPassword} from "../controllers/user/edit.controller"
-	import {editEmail} from "../controllers/user/edit.controller"
-	import {deleteUser} from "../controllers/user/delete.controller"
-	import {editAvatar} from "../controllers/user/avatar.controller";
-	import {deleteAvatar} from "../controllers/user/avatar.controller";
+// web
+import { dash } from "../controllers/web/dash.controller";
 
-	// game endpoints
-	// online users, game history, game options...
-	
+// user endpoints
+import { register } from "../controllers/user/register.controller";
+import { login } from "../controllers/user/login.controller";
+import { logout } from "../controllers/user/logout.controller";
+import { editUsername } from "../controllers/user/edit.controller"
+import { editPassword } from "../controllers/user/edit.controller"
+import { editEmail } from "../controllers/user/edit.controller"
+import { deleteUser } from "../controllers/user/delete.controller"
+import { editAvatar } from "../controllers/user/avatar.controller";
+import { deleteAvatar } from "../controllers/user/avatar.controller";
+
+// game endpoints
+// online users, game history, game options...
+import { getPlayer, updatePlayer } from "../controllers/game/player.controller";
+
 
 async function routes(fastify: FastifyInstance) {
-		
-	// dev
-	fastify.get('/', getHome);
-	fastify.get('/viewDB', viewDB);
-	fastify.get('/viewID', viewID);
-	
-	// web
-	fastify.get('/dashboard/:username', dash);
-	
-	// user management endpoints
-	fastify.post('/register', register);
-	fastify.post('/login', login);
-	fastify.get('/logout/:username', logout);
-	fastify.post('/editUsername/:username', editUsername);
-	fastify.post('/editPassword/:username', editPassword);
-	fastify.post('/editEmail/:username', editEmail);
-	fastify.delete('/delete/:username', deleteUser);
-	fastify.post('/editAvatar/:username', editAvatar);
-	fastify.post('/deleteAvatar/:username', deleteAvatar);
-	
-	// game endpoints
-	// online users, game history, game options...
+
+  // dev
+  fastify.get('/', getHome);
+  fastify.get('/viewDB', viewDB);
+  fastify.get('/viewID', viewID);
+
+  // web
+  fastify.get('/dashboard/:username', dash);
+
+  // user management endpoints
+  fastify.post('/register', register);
+  fastify.post('/login', login);
+  fastify.get('/logout/:username', logout);
+  fastify.post('/editUsername/:username', editUsername);
+  fastify.post('/editPassword/:username', editPassword);
+
+
+  fastify.post('/editEmail/:username', editEmail);
+  fastify.delete('/delete/:username', deleteUser);
+  fastify.post('/editAvatar/:username', editAvatar);
+  fastify.post('/deleteAvatar/:username', deleteAvatar);
+
+  // game endpoints
+  // online users, game history, game options...
+  fastify.get('/game/players/:username', getPlayer);
+  fastify.put('/game/players/:username', updatePlayer);
 
 };
 
