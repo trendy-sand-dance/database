c6e3125f8481919b21ae7257eef0010b0219de04
Sarah <115113929+smclacke@users.noreply.github.com>
2025-04-28 16:08:49 +0200

    Friends (#15)
    * figured friend schema out, making basic dev controllers to test
    
    * MAKING FRIENDS, changed it to use usernames not ids and broke it a bit, but we are getting somewhere woohoo
    
    * fixed the username unknown issue, handling friendships with usernames now not IDs
    
    * making sure only request receivers can accept the requests and not the senders
    
    * figuring out initiator checking
    
    * using IDs for friends, created endpoints for frontend, tested these and viewDB in dev, test actual endpoints with frontend
    
    * accept controller updating friendship status correctly
    
    * simplified friend schema futher, added reject delete request and block controller, blocking needs a lot of thought...
    
    * fixed the incorrect status vars, added view controllers for al friends and only friends
    
    * tested friend status updating and viewing endpoints, added win/loss stat endpoint
    
    * upddate stat controllers
    
    * fixed friend view controllers to get the friends on one specified user for the sidebar
    
    * chcked view friends controllers, tested with dev versions, working so far
    
    * include wins and losses in friend details for getAllFriends function
    
    * merged latest viewing friends stuff, added aceept/reject/block controllers for friends in frontend, ready to add to the html and test
    
    * database + frontend, view Players (of different relations) for sidebar, working on the ejs, pending results...
    
    * cleanedup friend vewiing controller
    
    * pr main lets go
    
    ---------
    
    Co-authored-by: smclacke <sarahmclacken@gmail.com>
    Co-authored-by: codingfrog27 <codingmerlin@gmail.com>

--- server/controllers/user/stats.controller.ts
+++ server/controllers/user/stats.controller.ts
@@ -6,13 +6,12 @@ export const getStats = async (request: FastifyRequest, reply: FastifyReply): Pr
 		const user = await request.server.prisma.user.findUnique({
 			where: { username },
 			select: {
-				// TODO: check whats needed here
 				wins: true, 
 				losses: true,
 			}
 		});
 
-	return reply.code(200).send(user);
+	return reply.code(200).send(user.wins, user.losses);
 	} catch (error) {
 		reply.status(500).send({ error: 'Failed to fetch user statistics' });
 	}
