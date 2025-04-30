f5fbdf38ec2fce2600d6233b307c091a35ac56ab
smclacke <sarahmclacken@gmail.com>
2025-04-30 11:51:30 +0200

    timezone correction, tested controllers in 'dev' mode

--- server/controllers/utils/matchUtils.controller.ts
+++ server/controllers/utils/matchUtils.controller.ts
@@ -1,9 +1,14 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 
-export function formatMatchDate(match: any) {
-  return {
-    ...match,
-    date: dayjs(match.date).format('YYYY-MM-DD HH:mm:ss'),
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export function formatMatchDate(match) {
+	return {
+	  ...match,
+	  date: dayjs.tz(match.date, "Europe/Berlin").format('YYYY-MM-DD HH:mm:ss'),
+	};
   };
-};
