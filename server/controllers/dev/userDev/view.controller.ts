09de7d8770507711cb5dfbc7f67fdf6b997e9dbd
smclacke <sarahmclacken@gmail.com>
2025-05-14 16:55:26 +0200

    chat db up and running, can view some default messages, lets gooo

--- server/controllers/dev/userDev/view.controller.ts
+++ server/controllers/dev/userDev/view.controller.ts
@@ -1,7 +1,6 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { formatChatDate, formatMatchDate } from '../../utils/dateUtils.controller';
 import { getAllFriends } from '../../utils/friendUtils.controller';
-//import { getAllChats } from 'chatDev.controller';
 
 export async function populate(request: FastifyRequest, reply: FastifyReply) {
 	try {
@@ -86,34 +85,61 @@ export const viewDB = async (request: FastifyRequest, reply: FastifyReply): Prom
 	}
   };
 
-//export const viewChat = async (request: FastifyRequest, reply: FastifyReply): Promise<any> => {
-//	try {
-//	  const allUsers = await request.server.prisma.user.findMany();
-
-//	  const users = await Promise.all(allUsers.map(async user => {
-//		  const chats = await getAllChats(user.id, request);
-//		  return { ...user, chats };
-//		//  const formattedChatDate = chats.map(formatChatDate); // how does this work viewing the date correctly?
-//		}));
-
-//		//const players = await request.server.prisma.player.findMany();
-	
-//		//const matches = await request.server.prisma.match.findMany();
-//		//const formattedMatches = matches.map(formatMatchDate);
-
-//		reply.send({ users: users });
-
-//	} catch (error) {
-//	  return reply.status(500).send({ error: 'Failed to fetch users' });
-//	}
-//  };
   
-export const viewID = async (request: FastifyRequest, reply: FastifyReply): Promise<any> => {
-	try {
-		const { id } = request.params as { id: string };
+  export const viewID = async (request: FastifyRequest, reply: FastifyReply): Promise<any> => {
+	  try {
+		  const { id } = request.params as { id: string };
 		const user = await request.server.prisma.user.findUnique({ where: { id: parseInt(id, 10) } });
 		reply.send(user);
 	} catch (error) {
 		reply.status(404).send({ error: 'Failed to fetch user via ID, user not found' });
 	}
+};
+
+
+export const viewChat = async (request: FastifyRequest, reply: FastifyReply): Promise<any> => {
+	try {
+		const allUsers = await request.server.prisma.user.findMany();
+
+		const users = await Promise.all(allUsers.map(async user => {
+			const chats = await getAllChats(user.id, request);
+			return { ...user, chats };
+		}));
+
+
+
+		reply.send({ users: users });
+
+	} catch (error) {
+		return reply.status(500).send({ error: 'Failed to fetch users' });
+	}
+	};
+
+export async function getAllChats(userId: number, request: FastifyRequest) {
+	const allChats = await request.server.prisma.chat.findMany({
+		where: {
+			OR: [
+				{ user1Id: userId },
+				{ user2Id: userId }
+			]
+		},
+		include: {
+			messages: true,
+			user1: { select: { id: true, username: true } },
+			user2: { select: { id: true, username: true } }
+		}
+	});
+	const chats = allChats.map(chat => {
+		const chatUser = chat.user1Id === userId ? chat.user2 : chat.user1;
+		return {
+			chatId: chat.id,
+			withUser: {
+				id: chatUser.id,
+				username: chatUser.username,
+			},
+			messages: chat.messages
+		};
+	}); 
+	return chats;
+
 };
