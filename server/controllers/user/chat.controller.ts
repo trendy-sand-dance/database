dd103d8b330d201c43bc9f32486df1edaafc6d8f
smclacke <sarahmclacken@gmail.com>
2025-05-27 19:49:55 +0200

    working endpoint to send chatms array of blocked users' ids

--- server/controllers/user/chat.controller.ts
+++ server/controllers/user/chat.controller.ts
@@ -1,16 +1,39 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { formatChatDate } from '../utils/dateUtils.controller';
 
+export async function getBlockedIds(user: number, request: FastifyRequest) {
+	const blocked = await request.server.prisma.friend.findMany({
+		where: {
+			status: { in: ['BLOCKED'] },
+			blocker: user,
+			OR: [
+				{ user1Id: user},
+				{ user2Id: user},
+			],
+		},
+		include: {
+			user1: true, 
+			user2: true,
+		},
+	});
+	const blockedPlayers = blocked.map(f => {
+		const blockedUser = f.user1Id === user ? f.user2 : f.user1;
+		return {
+				id: blockedUser.id,
+		};
+	});
+	return blockedPlayers;
+};
+
 export const getBlocked = async (request: FastifyRequest, reply: FastifyReply): Promise<any> => {
 	try {
-		const { userId } = request.params as { userId: Number };
+		const { userId } = request.params as { userId: number };
 		const user = Number(userId);
-		const blocked = await getBlocked(user, request);
+		const blocked = await getBlockedIds(user, request);
 		reply.send({ blocked });
 	} catch (error) {
-	reply.status(500).send({ error: 'Failed to fetch blocked users' });
+	reply.status(500).send({ error: 'Failed to fetch id\'s of blocked users' });
 	}
-	
 };
 
 // chat endpoints
