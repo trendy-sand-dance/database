c76c5f944d2e5408c8851d8a169a4a0796f68cc5
smclacke <sarahmclacken@gmail.com>
2025-06-05 20:04:39 +0200

    working that all messages between blocked players arent seen, not only from one side but all of them

--- server/controllers/user/chat.controller.ts
+++ server/controllers/user/chat.controller.ts
@@ -1,6 +1,30 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { formatChatDate } from '../utils/dateUtils.controller';
 
+export async function hasBeenBlocked(user: number, request: FastifyRequest) {
+	const whoBlockedMe = await request.server.prisma.friend.findMany({
+		where: {
+			status: { in: ['BLOCKED'] },
+			blocker: { not: user },
+			OR: [
+				{ user1Id: user},
+				{ user2Id: user},
+			],
+		},
+		include: {
+			user1: true, 
+			user2: true,
+		},
+	});
+	const beenBlocked = whoBlockedMe.map(f => {
+		const blocked = f.user1Id === user ? f.user2 : f.user1;
+		return {
+				id: blocked.id,
+		};
+	});
+	return beenBlocked;
+}
+
 export async function getBlockedIds(user: number, request: FastifyRequest) {
 	const blocked = await request.server.prisma.friend.findMany({
 		where: {
@@ -30,9 +54,26 @@ export const getBlocked = async (request: FastifyRequest, reply: FastifyReply): 
 		const { userId } = request.params as { userId: number };
 		const user = Number(userId);
 		const blocked = await getBlockedIds(user, request);
-		return reply.send(blocked.map(p => p.id));
+		const blocks = await hasBeenBlocked(user, request);
+		const allBlocks = [
+			...blocked.map(p => p.id),
+			...blocks.map(p => p.id)
+		];
+		return reply.send(allBlocks);
 	} catch (error) {
+		console.log(error);
 		return reply.status(500).send({ error: 'Failed to fetch id\'s of blocked users' });
 	}
 };
  
+export const getBlocks = async (request: FastifyRequest, reply: FastifyReply): Promise<any> => {
+	try {
+		const { userId } = request.params as { userId: number };
+		const user = Number(userId);
+		const blocks = await hasBeenBlocked(user, request);
+		return reply.send(blocks.map(p => p.id));
+	} catch (error) {
+		console.log(error);
+		return reply.status(500).send({ error: 'Failed to fetch id\'s of users who have blocked you' });
+	}
+};
